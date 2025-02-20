1edb8d01ca7b0fbbfb66110e49310db0a718c0dc
Benjamin Sergio <bssergio2@gmail.com>
2025-02-20 17:12:30 -0300

    Sorteio do Amigo secreto completo

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Benjamin Challenger Oracle One 8
+
+let listaAmigos = [];
+let textoListaSorteado = document.getElementById('listaAmigos');
+
+
+function  adicionarAmigo(){
+    let amigo = document.querySelector('input');
+
+    if(amigo.value != ''){
+        listaAmigos.push(amigo.value);
+        console.log("Amigo aadicionado!")
+        showListaAmigo(listaAmigos);
+    }
+    else{
+        console.log("Nescessario inserir um nome!");
+    }
+    const limpaAmigo = (amigo) => amigo.value = "";
+    limpaAmigo(amigo);
+    console.log(listaAmigos);
+    return;
+}
+
+function criaTexto(local,tipo, mensagem){
+    const texto = document.createElement(tipo);
+    texto.textContent = mensagem,
+    local.appendChild(texto);
+}
+
+function showListaAmigo(){
+    textoListaSorteado.innerHTML = '';
+    criaTexto(textoListaSorteado,'h3','Lista de Amigos')
+    listaAmigos.forEach(amigo => {
+        criaTexto(textoListaSorteado,'li',amigo);
+    });
+}
+
+function sortearAmigo(){
+
+    let limiteSorteio = listaAmigos.length;
+    if(limiteSorteio == 0){
+        alert("Lista de Amigos vazia, por favor Insira o nome dos amigos!")
+    }
+    let amigoSorteado =  parseInt(Math.random() * limiteSorteio );
+
+    const textoAmigoSorteado = document.getElementById('resultado');
+    textoAmigoSorteado.textContent = listaAmigos[amigoSorteado];
+
+    listaAmigos.splice(amigoSorteado,1);
+    showListaAmigo(listaAmigos)
+}
